f3d59068b03055ad48fe1feba672fcf7ee6c9593
kimjinsoo <1004kjs_@naver.com>
2022-02-10 14:20:59 +0900

    express 서버 생성

---
+++ server.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+
+app.get("/love", (req, res, next) => {
+    res.status(200).send("사랑해");
+});
+
+app.listen(3000, () => {
+    console.log("server is running");
+});
